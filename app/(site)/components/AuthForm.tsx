0fa7f7ec299484a2371b585fc34a2dbf82772361
strikermcs <tellermcs@gmail.com>
2023-06-26 19:15:42 +0300

    add mext auth and users page

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,18 +1,30 @@
 'use client';
 
+import axios from "axios";
 import Button from "@/app/components/UI/Button";
 import Input from "@/app/components/UI/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from 'react-icons/bs'
+import { toast } from "react-hot-toast";
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from "next/navigation";
 
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
+    const session = useSession()
+    const router = useRouter()
     const [variant, setVariant] = useState<Variant>('LOGIN')
     const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        if(session.status === 'authenticated') {
+            router.push('/users')
+        }
+    }, [session.status, router])
 
     const toggleVariant = useCallback(() => {
         if(variant === 'LOGIN') {
@@ -39,16 +51,41 @@ const AuthForm = () => {
         setIsLoading(true)
 
         if(variant === 'REGISTER') {
-
+            axios.post('/api/register', data)
+                .then(() => signIn('credentials', data))
+                .catch(() => toast.error('Somerhing went wrong!'))
+                .finally(() => setIsLoading(false))
         }
 
         if(variant === 'LOGIN') {
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            }).then((callback) => {
+                if(callback?.error) {
+                    toast.error('Invalid credentials')
+                }
 
+                if(callback?.ok && !callback.error) {
+                    toast.success('Logged in!')
+                }
+            }).finally(() => setIsLoading(false))
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true)
+
+        signIn(action, { redirect: false })
+            .then((callback) => {
+                if(callback?.error) {
+                    toast.error('Invalid credentials')
+                }
+
+                if(callback?.ok && !callback.error) {
+                    toast.success('Logged in!')
+                }
+            }).finally(() => setIsLoading(false))
     }
 
     return ( 
@@ -104,7 +141,7 @@ const AuthForm = () => {
                     </div>
                     <div className="mt-6 flex gap-2">
                         <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
-                        <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('github')} />
+                        <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
                     </div>        
                 </div>
                 <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
